8a7ac4acec7115935d0601cc230e2cdcf0b5e17e
AnasSekkat <Anas.Sekkat@student.ehb.be>
2021-08-16 02:42:18 +0200

    Added formating of unix timestamps

--- js/index.js
+++ js/index.js
@@ -1,15 +1,17 @@
 "use strict";
+
 import Article from './article.js';
+
 const news = {
   articles: [],
   init() {
     this.fetchArticles().then(data => {
-      
       this.createArticleInstances(data).then(
         this.renderArticles()
       )
     })
   },
+  
   async fetchArticles() {
     const response = await fetch('https://thecrew.cc/news/read.php');
     const data = await response.json();
@@ -22,16 +24,16 @@ const news = {
     });
     console.log(this.articles);
   },
+  
   renderArticles() {
     const container = document.getElementById('container');
     console.log(container);
     this.articles.forEach(article => {
-      console.log(article._titel);
       let HTMLstring = `
       <article>
         <header>
           <h2>${article._titel}</h2>
-          <h5>${article._datum}</h5>
+          <h5>${this.timeConverter(article._datum)}</h5>
         </header>
         <div class="body">
           <img
@@ -43,6 +45,19 @@ const news = {
     `;
       container.insertAdjacentHTML('beforeend', HTMLstring);
     });
+  },
+  
+  timeConverter(UNIX_timestamp) {
+    //functie gevonden op stackoverflow https://stackoverflow.com/questions/847185/convert-a-unix-timestamp-to-time-in-javascript
+    var a = new Date(UNIX_timestamp * 1000);
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min;
+    return time;
   }
 };
 
